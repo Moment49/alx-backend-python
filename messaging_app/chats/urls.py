6a39de502d18972edcf6f2480a21f56aaecced71
moment49 <ibenachoelvis49@gmail.com>
2025-07-24 08:34:22 +0100

    update:worked on the custom permissions for the message_app2

--- messaging_app/chats/urls.py
+++ messaging_app/chats/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path, include
 from . import views
 from rest_framework import routers 
+from rest_framework.routers import DefaultRouter as NestedDefaultRouter
 
 # Create a router and register our ViewSets with it.
-router = routers.DefaultRouter()
+# router = routers.DefaultRouter()
+router = NestedDefaultRouter()
+
 
 router.register(r'message', views.MessageViewSet, basename='message')
 router.register(r'conversation', views.ConversationViewSet, basename='conversation')
