80b0cdb68a91e1cf70828949a0c3ee3553c0b751
moment49 <ibenachoelvis49@gmail.com>
2025-07-27 16:30:15 +0100

    update: worked on the filter class and pagination

--- messaging_app/chats/pagination.py
+++ messaging_app/chats/pagination.py
@@ -1,4 +1,5 @@
 from rest_framework import pagination
+from rest_framework.response import Response 
 
 class CustomMessagePagination(pagination.PageNumberPagination):
     """This is a custom pagination to be used by the MessageViewSet class"""
@@ -6,3 +7,13 @@ class CustomMessagePagination(pagination.PageNumberPagination):
     page_size_query_param = 'page_size'
     max_page_size = 50
     page_query_param = 'p'
+
+    def get_paginated_response(self, data):
+        return Response({
+            'links': {
+                'next': self.get_next_link(),
+                'previous': self.get_previous_link()
+            },
+            'count': self.page.paginator.count,
+            'results': data
+        })
