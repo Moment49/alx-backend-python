3eb1e53e09cee8e75f89e5d364320a055d06bed4
moment49 <ibenachoelvis49@gmail.com>
2025-07-27 22:52:28 +0100

    updated:request.lognow

--- Django-Middleware-0x03/chats/middleware.py
+++ Django-Middleware-0x03/chats/middleware.py
@@ -39,4 +39,14 @@ class RequestLoggingMiddleware:
         # the view is called.
 
         return response
+    
+
+class RestrictAccessByTimeMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+
+        
+        response = self.get_response(request)
         
