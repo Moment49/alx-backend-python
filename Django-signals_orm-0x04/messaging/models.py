a690bf51a6e3afb81d4c463fd010c6e692fc777d
moment49 <ibenachoelvis49@gmail.com>
2025-08-03 14:18:58 +0100

    update: worked on the task0 for the signals

--- Django-signals_orm-0x04/messaging/models.py
+++ Django-signals_orm-0x04/messaging/models.py
@@ -6,8 +6,8 @@ CustomUser = get_user_model()
 # Create your models here.
 
 class Message(models.Model):
-    sender = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name="sender_messages")
-    reciever = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name="reciever_messages")
+    sender = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name="sender")
+    receiver = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name="receiver")
     content = models.TextField(max_length=300)
     timestamp = models.DateTimeField(auto_now=True)
     created_at = models.DateTimeField(auto_now_add=True)
